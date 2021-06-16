275911a2ad20c1a3a4234ce362fa0ed108c47433
Roman Medvedev <roman.medvedev@devdem.ru>
2021-06-17 01:57:50 +0300

    Start project

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Тех-лайн конфигуратор</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<script> var debug = true;</script>
+<body class="bg-secondary">
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Технолайн конфигуратор</a>
+    </div>
+</nav>
+<br>
+<div class="container bg-light" style="border-radius: 5px">
+    <h1 class="">Lorem</h1>
+</div>
+<script> if(debug) setTimeout(function(){ location.reload(); }, 5000); </script>
+</body>
+</html>
